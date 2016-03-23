f60d42e2608abe639c88a34b1aac35d734944258
Robowarrior834 <elovskijamie@aol.com>
2016-03-23 14:56:47 -0400

    Implimented opendialog

--- FibonacciEncyptonApp/FibonacciEncyptonApp/MainMenu.Designer.vb
+++ FibonacciEncyptonApp/FibonacciEncyptonApp/MainMenu.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class MainMenu
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -46,61 +46,61 @@ Partial Class Form1
         Me.lblName.AutoSize = True
         Me.lblName.Location = New System.Drawing.Point(22, 36)
         Me.lblName.Name = "lblName"
-        Me.lblName.Size = New System.Drawing.Size(53, 17)
+        Me.lblName.Size = New System.Drawing.Size(41, 13)
         Me.lblName.TabIndex = 0
         Me.lblName.Text = "Name: "
         '
         'lblPath
         '
         Me.lblPath.AutoSize = True
-        Me.lblPath.Location = New System.Drawing.Point(22, 68)
+        Me.lblPath.Location = New System.Drawing.Point(22, 66)
         Me.lblPath.Name = "lblPath"
-        Me.lblPath.Size = New System.Drawing.Size(49, 17)
+        Me.lblPath.Size = New System.Drawing.Size(38, 13)
         Me.lblPath.TabIndex = 1
         Me.lblPath.Text = "Path:  "
         '
         'lblSize
         '
         Me.lblSize.AutoSize = True
-        Me.lblSize.Location = New System.Drawing.Point(22, 105)
+        Me.lblSize.Location = New System.Drawing.Point(22, 92)
         Me.lblSize.Name = "lblSize"
-        Me.lblSize.Size = New System.Drawing.Size(43, 17)
+        Me.lblSize.Size = New System.Drawing.Size(33, 13)
         Me.lblSize.TabIndex = 2
         Me.lblSize.Text = "Size: "
         '
         'lblDateMod
         '
         Me.lblDateMod.AutoSize = True
-        Me.lblDateMod.Location = New System.Drawing.Point(22, 138)
+        Me.lblDateMod.Location = New System.Drawing.Point(22, 119)
         Me.lblDateMod.Name = "lblDateMod"
-        Me.lblDateMod.Size = New System.Drawing.Size(99, 17)
+        Me.lblDateMod.Size = New System.Drawing.Size(75, 13)
         Me.lblDateMod.TabIndex = 3
         Me.lblDateMod.Text = "Date modified:"
         '
         'lblDateCreated
         '
         Me.lblDateCreated.AutoSize = True
-        Me.lblDateCreated.Location = New System.Drawing.Point(22, 172)
+        Me.lblDateCreated.Location = New System.Drawing.Point(22, 140)
         Me.lblDateCreated.Name = "lblDateCreated"
-        Me.lblDateCreated.Size = New System.Drawing.Size(96, 17)
+        Me.lblDateCreated.Size = New System.Drawing.Size(73, 13)
         Me.lblDateCreated.TabIndex = 4
         Me.lblDateCreated.Text = "Date Created:"
         '
         'lblOwner
         '
         Me.lblOwner.AutoSize = True
-        Me.lblOwner.Location = New System.Drawing.Point(22, 200)
+        Me.lblOwner.Location = New System.Drawing.Point(22, 164)
         Me.lblOwner.Name = "lblOwner"
-        Me.lblOwner.Size = New System.Drawing.Size(53, 17)
+        Me.lblOwner.Size = New System.Drawing.Size(41, 13)
         Me.lblOwner.TabIndex = 5
         Me.lblOwner.Text = "Owner:"
         '
         'lblKeyNum
         '
         Me.lblKeyNum.AutoSize = True
-        Me.lblKeyNum.Location = New System.Drawing.Point(22, 227)
+        Me.lblKeyNum.Location = New System.Drawing.Point(22, 186)
         Me.lblKeyNum.Name = "lblKeyNum"
-        Me.lblKeyNum.Size = New System.Drawing.Size(86, 17)
+        Me.lblKeyNum.Size = New System.Drawing.Size(65, 13)
         Me.lblKeyNum.TabIndex = 6
         Me.lblKeyNum.Text = "Key Number"
         '
@@ -110,7 +110,7 @@ Partial Class Form1
         Me.lblName2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.lblName2.Location = New System.Drawing.Point(213, 36)
         Me.lblName2.Name = "lblName2"
-        Me.lblName2.Size = New System.Drawing.Size(77, 19)
+        Me.lblName2.Size = New System.Drawing.Size(61, 15)
         Me.lblName2.TabIndex = 7
         Me.lblName2.Text = "Word_Doc"
         '
@@ -118,9 +118,9 @@ Partial Class Form1
         '
         Me.lblPath2.AutoSize = True
         Me.lblPath2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblPath2.Location = New System.Drawing.Point(213, 68)
+        Me.lblPath2.Location = New System.Drawing.Point(213, 64)
         Me.lblPath2.Name = "lblPath2"
-        Me.lblPath2.Size = New System.Drawing.Size(281, 19)
+        Me.lblPath2.Size = New System.Drawing.Size(222, 15)
         Me.lblPath2.TabIndex = 8
         Me.lblPath2.Text = "C:\Users\student-19\Desktop\Word_Doc.txt"
         '
@@ -128,9 +128,9 @@ Partial Class Form1
         '
         Me.lblSize2.AutoSize = True
         Me.lblSize2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblSize2.Location = New System.Drawing.Point(213, 105)
+        Me.lblSize2.Location = New System.Drawing.Point(213, 90)
         Me.lblSize2.Name = "lblSize2"
-        Me.lblSize2.Size = New System.Drawing.Size(73, 19)
+        Me.lblSize2.Size = New System.Drawing.Size(56, 15)
         Me.lblSize2.TabIndex = 9
         Me.lblSize2.Text = "570 Bytes"
         '
@@ -138,9 +138,9 @@ Partial Class Form1
         '
         Me.lblDateMod2.AutoSize = True
         Me.lblDateMod2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblDateMod2.Location = New System.Drawing.Point(213, 138)
+        Me.lblDateMod2.Location = New System.Drawing.Point(213, 119)
         Me.lblDateMod2.Name = "lblDateMod2"
-        Me.lblDateMod2.Size = New System.Drawing.Size(125, 19)
+        Me.lblDateMod2.Size = New System.Drawing.Size(99, 15)
         Me.lblDateMod2.TabIndex = 10
         Me.lblDateMod2.Text = "1/16/2016 1:16pm"
         '
@@ -148,9 +148,9 @@ Partial Class Form1
         '
         Me.lblDateCreated2.AutoSize = True
         Me.lblDateCreated2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblDateCreated2.Location = New System.Drawing.Point(213, 172)
+        Me.lblDateCreated2.Location = New System.Drawing.Point(213, 140)
         Me.lblDateCreated2.Name = "lblDateCreated2"
-        Me.lblDateCreated2.Size = New System.Drawing.Size(125, 19)
+        Me.lblDateCreated2.Size = New System.Drawing.Size(99, 15)
         Me.lblDateCreated2.TabIndex = 11
         Me.lblDateCreated2.Text = "2/16/2016 2:16pm"
         '
@@ -158,23 +158,23 @@ Partial Class Form1
         '
         Me.lblOwner2.AutoSize = True
         Me.lblOwner2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblOwner2.Location = New System.Drawing.Point(213, 200)
+        Me.lblOwner2.Location = New System.Drawing.Point(213, 164)
         Me.lblOwner2.Name = "lblOwner2"
-        Me.lblOwner2.Size = New System.Drawing.Size(80, 19)
+        Me.lblOwner2.Size = New System.Drawing.Size(61, 15)
         Me.lblOwner2.TabIndex = 12
         Me.lblOwner2.Text = "Student-19"
         '
         'txtKeyNumber
         '
-        Me.txtKeyNumber.Location = New System.Drawing.Point(216, 227)
+        Me.txtKeyNumber.Location = New System.Drawing.Point(213, 186)
         Me.txtKeyNumber.Name = "txtKeyNumber"
-        Me.txtKeyNumber.Size = New System.Drawing.Size(120, 22)
+        Me.txtKeyNumber.Size = New System.Drawing.Size(120, 20)
         Me.txtKeyNumber.TabIndex = 13
         Me.txtKeyNumber.Text = "123456"
         '
         'btnFileSelection
         '
-        Me.btnFileSelection.Location = New System.Drawing.Point(182, 279)
+        Me.btnFileSelection.Location = New System.Drawing.Point(147, 225)
         Me.btnFileSelection.Name = "btnFileSelection"
         Me.btnFileSelection.Size = New System.Drawing.Size(164, 46)
         Me.btnFileSelection.TabIndex = 14
@@ -183,7 +183,7 @@ Partial Class Form1
         '
         'btnEncyption
         '
-        Me.btnEncyption.Location = New System.Drawing.Point(97, 331)
+        Me.btnEncyption.Location = New System.Drawing.Point(66, 277)
         Me.btnEncyption.Name = "btnEncyption"
         Me.btnEncyption.Size = New System.Drawing.Size(164, 46)
         Me.btnEncyption.TabIndex = 15
@@ -192,7 +192,7 @@ Partial Class Form1
         '
         'btnDecyption
         '
-        Me.btnDecyption.Location = New System.Drawing.Point(267, 331)
+        Me.btnDecyption.Location = New System.Drawing.Point(236, 277)
         Me.btnDecyption.Name = "btnDecyption"
         Me.btnDecyption.Size = New System.Drawing.Size(164, 46)
         Me.btnDecyption.TabIndex = 17
@@ -202,7 +202,7 @@ Partial Class Form1
         'Form1
         '
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
-        Me.ClientSize = New System.Drawing.Size(528, 392)
+        Me.ClientSize = New System.Drawing.Size(467, 337)
         Me.Controls.Add(Me.btnDecyption)
         Me.Controls.Add(Me.btnEncyption)
         Me.Controls.Add(Me.btnFileSelection)
