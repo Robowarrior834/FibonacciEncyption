2f56c65cf2e1db58be9bf880eab16ca14b22a0e2
Scott Carpency <sc1558@desales.edu>
2016-04-13 14:01:30 -0400

    Misc. changes

--- FibonacciEncyptonApp/FibonacciEncyptonApp/My Project/AssemblyInfo.vb
+++ FibonacciEncyptonApp/FibonacciEncyptonApp/My Project/AssemblyInfo.vb
@@ -8,10 +8,10 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("Fibonacci Text Enctyition")>
+<Assembly: AssemblyTitle("Fibonacci Text Encryption")>
 <Assembly: AssemblyDescription("This program takes a text file and encrypts the data in a secure and quick manner.")>
 <Assembly: AssemblyCompany("A.C.E. Computing")>
-<Assembly: AssemblyProduct("Fibonacci Text Encypton")>
+<Assembly: AssemblyProduct("Fibonacci Text Encrypton")>
 <Assembly: AssemblyCopyright("Copyright © A.C.E. Computing 2016")>
 <Assembly: AssemblyTrademark("")>
 
